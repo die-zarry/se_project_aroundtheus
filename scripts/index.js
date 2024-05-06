32647fea5fb09e94f79718f78e60c156f1dc4fa3
Zarry Die <zarrydie03@gmail.com>
2024-05-05 22:29:46 -0400

    fixing the array format

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,26 @@
 let initialCards = [
-  { emre: "https://unsplash.com/photos/a-table-and-chairs-in-a-room-with-a-book-shelf-YIOnWHJYm1w" },
-  { eren: "https://unsplash.com/photos/a-pathway-leading-to-a-field-of-flowers-Hj2J0pG5ktg" },
-  { mariovonRotz: "https://unsplash.com/photos/a-man-holding-a-surfboard-standing-on-top-of-a-beach-1pcMSzyNPyg" },
-  { mokuenko: "https://unsplash.com/photos/a-view-of-the-ocean-from-a-beach-pL5dNKZbDPM" },
-  { avecalvar: "https://unsplash.com/photos/background-pattern-Lxqn8BoJsMw" },
-  { rafaelgarcin: "https://unsplash.com/photos/a-solar-eclipse-is-seen-in-the-dark-sky-9qxB694vcBI" },
+  {
+    title: "emre",
+    link: "https://unsplash.com/photos/a-table-and-chairs-in-a-room-with-a-book-shelf-YIOnWHJYm1w",
+  },
+  {
+    title: "eren",
+    link: "https://unsplash.com/photos/a-pathway-leading-to-a-field-of-flowers-Hj2J0pG5ktg",
+  },
+  {
+    title: "mariovonRotz",
+    link: "https://unsplash.com/photos/a-man-holding-a-surfboard-standing-on-top-of-a-beach-1pcMSzyNPyg",
+  },
+  {
+    title: "mokuenko",
+    link: "https://unsplash.com/photos/a-view-of-the-ocean-from-a-beach-pL5dNKZbDPM",
+  },
+  {
+    title: "avecalvar",
+    link: "https://unsplash.com/photos/background-pattern-Lxqn8BoJsMw",
+  },
+  {
+    title: "rafaelgarcin",
+    link: "https://unsplash.com/photos/a-solar-eclipse-is-seen-in-the-dark-sky-9qxB694vcBI",
+  },
 ];
