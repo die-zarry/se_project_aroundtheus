aaa22ee49ac14292e68a6583c159cfcaaf8c3999
Zarry Die <zarrydie03@gmail.com>
2024-05-05 22:50:20 -0400

    formating the array

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 let initialCards = [
   {
-    title: "emre",
-    link: "https://unsplash.com/photos/a-table-and-chairs-in-a-room-with-a-book-shelf-YIOnWHJYm1w",
+    title: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
   {
-    title: "eren",
-    link: "https://unsplash.com/photos/a-pathway-leading-to-a-field-of-flowers-Hj2J0pG5ktg",
+    title: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
   {
-    title: "mariovonRotz",
-    link: "https://unsplash.com/photos/a-man-holding-a-surfboard-standing-on-top-of-a-beach-1pcMSzyNPyg",
+    title: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
   {
-    title: "mokuenko",
-    link: "https://unsplash.com/photos/a-view-of-the-ocean-from-a-beach-pL5dNKZbDPM",
+    title: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
   {
-    title: "avecalvar",
-    link: "https://unsplash.com/photos/background-pattern-Lxqn8BoJsMw",
+    title: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
   {
-    title: "rafaelgarcin",
-    link: "https://unsplash.com/photos/a-solar-eclipse-is-seen-in-the-dark-sky-9qxB694vcBI",
+    title: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
