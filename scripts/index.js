f15dd479c36200ada0ad5a70743bfd93de19557b
Zarry Die <zarrydie03@gmail.com>
2024-05-11 07:00:29 -0400

    formating the object names

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 let initialCards = [
   {
-    title: "MiladaVigero",
+    name: "MiladaVigero",
     link: "https://unsplash.com/photos/yellow-corn-in-close-up-photography-r7NhxSjz7e4",
   },
   {
-    title: "GettyImages",
+    name: "GettyImages",
     link: "https://unsplash.com/photos/creative-layout-of-fresh-fruits-nuts-and-ice-cream-cone-flat-lay-summer-concept-Hjv_HtwB9-Y",
   },
   {
-    title: "KarolinaGrabowska",
+    name: "KarolinaGrabowska",
     link: "https://unsplash.com/photos/a-person-cutting-a-plate-with-a-knife-and-fork-1huxR1ls2GI",
   },
   {
-    title: "PatriciaJadach",
+    name: "PatriciaJadach",
     link: "https://unsplash.com/photos/a-bunch-of-raspberries-sitting-on-top-of-each-other-jZU3x1Rwlqw",
   },
   {
-    title: "Josefin",
+    name: "Josefin",
     link: "https://unsplash.com/photos/lemon-avocado-ginger-orange-fruit-bananas-and-calamdin-s0fuB1h3yPw",
   },
   {
-    title: "Rayia Soserberg",
+    name: "Rayia Soserberg",
     link: "https://unsplash.com/photos/a-cutting-board-topped-with-sliced-oranges-next-to-a-knife-ev_GpmUPOwo",
   },
 ];
